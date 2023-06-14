96be131b76b131999de3a64ce723d0833da4f9ab
aadil124 <ansariaadil124@gmail.com>
2023-06-14 20:54:11 +0530

    updated data

--- fetchData.js
+++ fetchData.js
@@ -1,5 +1,4 @@
 const btn = document.getElementById("button1");
-// const url = "https://api.breakingbadquotes.xyz/v1/quotes";
 const url = "https://randomuser.me/api/?results=50&gender=female";
 const card = document.querySelector(".cardItem");
 
@@ -8,26 +7,22 @@ btn.addEventListener("click", async function getData() {
   try {
     let res = await fetch(url);
     let jsonobj = await res.json();
-    // console.log(jsonobj.results);
     let data = jsonobj.results;
     let output = "";
     data.map((item) => {
-      console.log(item);
       let picture = item.picture.large;
       let name = item.name.first;
 
       output += `
-        <div class="col">
-            <div class="card h-100">
+        <div class="col border-dark">
+            <div class="card h-80">
                 <img src="${picture}" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title">${name}</h5>
-                    <p class="card-text">${item.dob.age}</p>
-                    <ul class="list-group list-group-flush">
-                    <li class="list-group-item">Email: ${item.email}</li>
-                    <li class="list-group-item">Country: ${item.location.country}</li>
-                    <li class="list-group-item">Mob: ${item.cell}</li>
-                    </ul>
+                <div class="card-body text-success">
+                    <h5 class="card-title">Name: ${name}</h5>
+                    <p class="card-text">Age: ${item.dob.age}</p>
+                    <p class="card-text">Email: ${item.email}</p>
+                    <p class="card-text">Country: ${item.location.country}</p>
+                    <p class="card-text">Mob: ${item.cell}</p>
                 </div>
             </div>
         </div>
@@ -38,12 +33,3 @@ btn.addEventListener("click", async function getData() {
     console.log(error);
   }
 });
-
-// function getData() {
-//   fetch(url)
-//     .then((res) => res.json())
-//     .then((data) => console.log(data))
-//     .catch((err) => {
-//       console.log(err);
-//     });
-// }
