53457d420ecc7534015950ee3045cb8291071f27
aadil124 <ansariaadil124@gmail.com>
2023-06-17 21:25:50 +0530

    updted code

--- fetchData.js
+++ fetchData.js
@@ -4,30 +4,41 @@ const urlfemale = "https://randomuser.me/api/?results=50&gender=female";
 const urlmale = "https://randomuser.me/api/?results=50&gender=male";
 const card = document.querySelector(".cardItem");
 
-btnMale.addEventListener("click", async function getData() {
+btnMale.addEventListener("click", () => {
+  getData(urlmale);
+});
+btnFemale.addEventListener("click", () => {
+  getData(urlfemale);
+});
+
+async function getData(url) {
   try {
-    let res = await fetch(urlmale);
+    let res = await fetch(url);
     let jsonobj = await res.json();
     let data = jsonobj.results;
     let output = "";
     data.map((item) => {
       let picture = item.picture.large;
       let name = item.name.first;
+      let age = item.dob.age;
+      let email = item.email;
+      let country = item.location.country;
+      let mobileNumber = item.cell;
 
       output += `
-        <div class="col text-info">
-            <div class="card h-80">
-                <img src="${picture}" class="card-img-top" alt="...">
-                <div class="card-header">
-                   <h5 class="card-title">Name: ${name}</h5>
-                    <p class="card-text">Age: ${item.dob.age}</p>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">Email: ${item.email}</p>
-                    <p class="card-text">Country: ${item.location.country}</p>
-                    <p class="card-text">Mob: ${item.cell}</p>
-                </div>
-            </div>
+        <div class="col">
+        <div class="card text-white bg-dark h-100">
+        <div class="card-header">
+        <img src="${picture}" class="card-img-top" alt="...">
+        <h5 class="card-title mt-2">${name}</h5>
+        </div>
+        <div class="card-body">
+        <p class="card-text">Age: ${age}</p>
+        <p class="card-text">Email: ${email}</p>
+        <p class="card-text">Country: ${country}</p>
+        <p class="card-text">Mob: ${mobileNumber}</p>
+        </div>
+        </div>
         </div>
       `;
       card.innerHTML = output;
@@ -35,37 +46,4 @@ btnMale.addEventListener("click", async function getData() {
   } catch (error) {
     console.log(error);
   }
-});
-
-btnFemale.addEventListener("click", async function getData() {
-  try {
-    let res = await fetch(urlfemale);
-    let jsonobj = await res.json();
-    let data = jsonobj.results;
-    let output = "";
-    data.map((item) => {
-      let picture = item.picture.large;
-      let name = item.name.first;
-
-      output += `
-        <div class="col text-info">
-            <div class="card h-80">
-                <img src="${picture}" class="card-img-top" alt="...">
-                <div class="card-header">
-                   <h5 class="card-title">Name: ${name}</h5>
-                    <p class="card-text">Age: ${item.dob.age}</p>
-                </div>
-                <div class="card-body">
-                    <p class="card-text">Email: ${item.email}</p>
-                    <p class="card-text">Country: ${item.location.country}</p>
-                    <p class="card-text">Mob: ${item.cell}</p>
-                </div>
-            </div>
-        </div>
-      `;
-      card.innerHTML = output;
-    });
-  } catch (error) {
-    console.log(error);
-  }
-});
+}
